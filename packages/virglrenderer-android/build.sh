54390cd02e67d2c8064561f8fcbb84e97f056550
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2026-09-25 21:47:20 +0530

    debug(virglrenderer-android): report worker exits

--- packages/virglrenderer-android/build.sh
+++ packages/virglrenderer-android/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@licy183"
 TERMUX_PKG_VERSION="1.3.0"
 _LIBEPOXY_VERSION="1.5.10"
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_REVISION=4
 
 TERMUX_PKG_SRCURL=(
 	https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
