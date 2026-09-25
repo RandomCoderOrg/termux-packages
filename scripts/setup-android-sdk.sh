6ba5b7c9c09ede190702ae1b531f244bb699fac4
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2026-09-25 21:47:20 +0530

    ci: rebuild Android renderer with fork toolchain

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Keep fork package builds synchronized with the NDK selected by properties.sh.
+# Keep fork Android renderer builds synchronized with the NDK selected by properties.sh.
 
 set -e -u
 
